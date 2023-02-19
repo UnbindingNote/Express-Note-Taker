95f5115e3fb1fde9edbed232217f028e1cb65822
UnbindingNote <[200~111218283+UnbindingNote@users.noreply.github.com~>
2023-02-19 02:40:05 -0500

    final routing, just need readme to add.

--- routes/api.js
+++ routes/api.js
@@ -3,28 +3,28 @@ const unique = require("../db/unique");
 
 // GET "/api/notes" provides all notes from db
 router.get("/notes", (req, res) => {
-    // unique
-    // .getNotes()
-    // .then((notes) => {
-    //   return res.json(notes);
-    // })
-    // .catch((err) => res.status(500).json(err));
+    unique
+    .getNotes()
+    .then((notes) => {
+      return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 // POST "/api/notes" adds new note to db
 router.post("/notes", (req, res) => {
-    // unique
-    // .addNote(req.body)
-    // .then((note) => res.json(note))
-    // .catch((err) => res.status(500).json(err));
+    unique
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
 });
 
 // DELETE "/api/notes" delete a note from db matching id.
 router.delete("/notes/:id", (req, res) => {
-    // unique
-    // .removeNote(req.params.id)
-    // .then(() => res.json({ ok: true }))
-    // .catch((err) => res.status(500).json(err));
+    unique
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
--- routes/html.js
+++ routes/html.js
@@ -1,10 +1,12 @@
-const path = require('path');
-const router = require('express').Router();
+const path = require("path");
+const router = require("express").Router();
 
 // "/notes" use notes.html
-router.get('/notes', (req, res) => {
-  });
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
-  // anything else, use index.html
-router.get('*', (req, res) => {
-  });
+// anything else, use index.html
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
